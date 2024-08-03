bd02359b0d30a706660d496151316065b5744164
Leon Zündel <leon.zuendel@pooliestudios.com>
2024-08-04 00:47:08 +0200

    fixes header padding

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Banner() {
 	return (
-		<div className="banner w-full h-10 flex justify-center items-center gap-4 bg-primary">
+		<div className="banner w-full h-10 flex justify-center items-center gap-4 bg-primary px-5">
 			<Link href="mailto:mail@red-influence.com">mail@red-influence.com</Link>
 		</div>
 	);
