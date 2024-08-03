709e249dac5d5e06ecca9f9e1498ec0dd902c23e
Leon Zündel <leon.zuendel@pooliestudios.com>
2024-08-03 23:39:13 +0200

    fixes support for netlify forms

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,22 @@
 import { Button } from '@nextui-org/button';
 import { Input, Textarea } from '@nextui-org/input';
 import Link from 'next/link';
+import { FormEvent } from 'react';
 
 export default function Contact() {
+	const handleFormSubmit = async (event: FormEvent) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.target as HTMLFormElement);
+		await fetch('/__forms.html', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: new URLSearchParams(formData as any).toString(),
+		});
+
+		(event.target as HTMLFormElement).reset();
+	};
+
 	return (
 		<div className="contact bg-background section" id="contact">
 			<div className="container py-20 flex flex-col gap-y-10 mx-auto items-center px-5 md:px-0">
@@ -14,19 +28,24 @@ export default function Contact() {
 					name="contact"
 					method="POST"
 					action="/"
+					onSubmit={handleFormSubmit}
 				>
+					<Input type="hidden" name="form-name" value="contact" />
+
 					<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
-						<Input type="text" label="Vorname" size="lg" />
-						<Input type="text" label="Nachname" size="lg" />
+						<Input type="text" label="Vorname" name="vorname" size="lg" />
+						<Input type="text" label="Nachname" name="nachname" size="lg" />
 						<Input
 							type="email"
 							label="Email"
+							name="email"
 							className="md:col-span-2"
 							size="lg"
 						/>
 
 						<Textarea
 							label="Nachricht"
+							name="nachricht"
 							className="md:col-span-2"
 							minRows={8}
 							size="lg"
