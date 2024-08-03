0a1c0e1372ac84b2a46fc8f178d8175be3cd3eb1
Leon Zündel <leon.zuendel@pooliestudios.com>
2024-08-03 23:02:42 +0200

    refactors contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
 		<div className="contact bg-background section" id="contact">
 			<div className="container py-20 flex flex-col gap-y-10 mx-auto items-center px-5 md:px-0">
 				<h2 className="text-3xl md:text-6xl">Kontaktiere uns jetzt</h2>
-				<div className="w-full max-w-6xl flex flex-col gap-10 items-start">
+				<form className="w-full max-w-6xl flex flex-col gap-10 items-start">
 					<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
 						<Input type="text" label="Vorname" size="lg" />
 						<Input type="text" label="Nachname" size="lg" />
@@ -25,8 +25,8 @@ export default function Contact() {
 							size="lg"
 						/>
 					</div>
-					<Button>Nachricht absenden</Button>
-				</div>
+					<Button type="submit">Nachricht absenden</Button>
+				</form>
 
 				<Link href="mailto:mail@red-influence.com" className="text-xl">
 					Oder einfach per Mail.
