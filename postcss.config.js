272d6951e8669dcab396b2b56f99e9b5161182a4
Leon Zündel <leon.zuendel@pooliestudios.com>
2024-08-05 22:42:56 +0200

    switches back to cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,6 @@ module.exports = {
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production'
-			? {
-					'@fullhuman/postcss-purgecss': {
-						content: [
-							'./pages/**/*.{js,jsx,ts,tsx}',
-							'./components/**/*.{js,jsx,ts,tsx}',
-						],
-						defaultExtractor: (content) =>
-							content.match(/[\w-/:]+(?<!:)/g) || [],
-						safelist: ['html', 'body'],
-					},
-			  }
-			: {}),
+		...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
 	},
 };
