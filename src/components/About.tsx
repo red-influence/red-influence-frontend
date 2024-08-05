ca065b1a8163df5f69254c11403d5bb0d72a5d85
Leon Zündel <leon.zuendel@pooliestudios.com>
2024-08-05 18:13:55 +0200

    adds socials to person box

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import { About as AboutType, Person } from '@/types/sanity.types';
 import { Image } from '@nextui-org/image';
 import { imageToUrl } from '@/utils/sanity';
 import ScrollAnimation from '@/components/ScrollAnimation';
-import { Card, CardBody } from '@nextui-org/card';
+import PersonBox from './PersonBox';
 
 export default function About({
 	title,
@@ -52,37 +52,7 @@ export default function About({
 				<div className="flex gap-3 md:gap-6 flex-wrap">
 					{persons.map((person, index) =>
 						person.description ? (
-							<Card
-								className="py-4 bg-transparent md:bg-background/60 md:dark:bg-default-100/50 w-full md:max-w-2xl"
-								shadow="none"
-								isBlurred
-								key={index}
-							>
-								<CardBody className="overflow-visible py-2 flex flex-col md:flex-row gap-5 items-center md:items-start">
-									<Image
-										alt={person.title}
-										className="object-cover rounded-xl max-w-52"
-										src={imageToUrl(person.image).height(500).url()}
-										width={200}
-										height={250}
-									/>
-
-									<div className="flex flex-col gap-5 items-center md:items-start">
-										<div className="flex flex-col gap-2">
-											<h3 className="text-lg md:text-xl text-center md:text-left">
-												{person.title}
-											</h3>
-											<h4 className="text-sm md:text-medium text-center md:text-left">
-												{person.position}
-											</h4>
-										</div>
-
-										<p className="text-medium md:text-lg !leading-loose flex flex-col gap-y-7 md:pe-5 text-center md:text-left">
-											{person.description}
-										</p>
-									</div>
-								</CardBody>
-							</Card>
+							<PersonBox person={person} key={index} />
 						) : null
 					)}
 				</div>
---
+++ src/components/PersonBox.tsx
@@ -0,0 +1,62 @@
+import { Person } from '@/types/sanity.types';
+import { imageToUrl } from '@/utils/sanity';
+import { Card, CardBody } from '@nextui-org/card';
+import { Image } from '@nextui-org/image';
+import Link from 'next/link';
+
+export default function PersonBox({ person }: { person: Person }) {
+	return (
+		<Card
+			className="py-4 bg-transparent md:bg-background/60 md:dark:bg-default-100/50 w-full md:max-w-2xl"
+			shadow="none"
+			isBlurred
+		>
+			<CardBody className="overflow-visible py-2 flex flex-col md:flex-row gap-5 items-center md:items-start">
+				<Image
+					alt={person.title}
+					className="object-cover rounded-xl max-w-52"
+					src={imageToUrl(person.image).height(500).url()}
+					width={200}
+					height={250}
+				/>
+
+				<div className="flex flex-col gap-5 items-center md:items-start">
+					<div className="flex flex-col gap-2">
+						<h3 className="text-lg md:text-xl text-center md:text-left">
+							{person.title}
+						</h3>
+						<h4 className="text-sm md:text-medium text-center md:text-left">
+							{person.position}
+						</h4>
+					</div>
+
+					<p className="text-medium md:text-lg !leading-loose flex flex-col gap-y-7 md:pe-5 text-center md:text-left">
+						{person.description}
+					</p>
+
+					{person.socials && (
+						<div className="flex gap-y-2 gap-x-5 flex-wrap">
+							{person.socials.map((social, index) => (
+								<div
+									className="flex gap-1 hover:underline items-center opacity-60"
+									key={index}
+								>
+									<Image
+										alt={social.type}
+										src={`/${social.type.toLowerCase()}.svg`}
+										width={30}
+										height={30}
+									/>
+
+									<Link href={social.url} target="_blank">
+										{social.type}
+									</Link>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+			</CardBody>
+		</Card>
+	);
+}
